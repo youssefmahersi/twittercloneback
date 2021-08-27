8117a0d9d402a19992cea73db6a26bf475d0c87d
youssef <youssefmahersi0@gmail.com>
2021-08-27 23:59:07 +0100

    make it better

--- app.js
+++ app.js
@@ -43,11 +43,9 @@ app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(cors());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
-  );
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type',' Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
 //routes
