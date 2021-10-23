5f5b0fe5e9bcba436f49470c9dc38504af8bf9b2
youssef <youssefmahersi0@gmail.com>
2021-10-23 13:48:31 +0100

    fix the header

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', ['Content-Type',' Authorization']);
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
